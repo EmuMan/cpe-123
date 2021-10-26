3e449a56772fa0472d0427da4b6beb71b2b54b33
EmuMan (Jacob Kelleran) <jacob.kelleran@outlook.com>
2021-10-26 14:15:12 -0700

    Mostly finished lab3

--- generator.py
+++ generator.py
@@ -11,6 +11,8 @@ bl_info = {
     "category": "Import-Export"
 }
 
+import json
+
 import bpy
 from bpy.props import (StringProperty,
                        PointerProperty,
@@ -146,66 +148,61 @@ class JSONExport(Operator):
         scene = context.scene
         tool = scene.p5js_json_tool
 
-        final_lines = []
+        final = {}
 
-        with open(tool.template_filepath, "rt") as f:
-            template_lines = f.readlines()
+        for collection in bpy.data.collections:
 
-        for line in template_lines:
-            for collection in bpy.data.collections:
-                if f"%%{collection.name}%%" in line:
-                    indentation = ""
-                    for char in line:
-                        if char == " " or char == "\t": indentation += char
-                        else: break
-
-                    for object in collection.objects:
-                        if "camera" in object.name and not object.name.startswith("obj_"):
-                            camera_up = object.matrix_world.to_quaternion() @ Vector((0.0, -1.0, 0.0))
-                            camera_forward = object.matrix_world.to_quaternion() @ Vector((0.0, 0.0, -1.0))
-                            camera_pointing = object.location + camera_forward
-                            final_lines.append(indentation + 
-                                    f"camera({-object.location.x}, {object.location.y}, {object.location.z}, {-camera_pointing.x}, {camera_pointing.y}, {camera_pointing.z}, {-camera_up.x}, {camera_up.y}, {camera_up.z});")
-                            final_lines.append("\n")
+            final[collection.name] = []
                     
-                    for object in collection.objects:
-                        if object.name.startswith("obj_"):
-                            object_type = object.name[4:]
-                            try:
-                                color = [v * 255 for v in object.active_material.node_tree.nodes[1].inputs[0].default_value[:3]]
-                            except AttributeError:
-                                color = [205, 205, 205]
-                            final_lines.append(indentation + "push();\n")
-                            final_lines.append(indentation + f"translate({-object.location.x}, {object.location.y}, {object.location.z});\n")
-                            final_lines.append(indentation + f"rotateZ({-object.rotation_euler.z});\n")
-                            final_lines.append(indentation + f"rotateY({-object.rotation_euler.y});\n")
-                            x_rotation = object.rotation_euler.x + (3.14159265 / 2 if "box" in object_type or "cylinder" in object_type or "cone" in object_type else 0)
-                            final_lines.append(indentation + f"rotateX({x_rotation});\n")
-                            final_lines.append(indentation + f"fill({color[0]}, {color[1]}, {color[2]});\n")
-                            if "box" in object_type:
-                                final_lines.append(indentation + f"box({object.dimensions.x}, {object.dimensions.z}, {object.dimensions.y});\n")
-                            elif "plane" in object_type:
-                                final_lines.append(indentation + f"plane({object.dimensions.x}, {object.dimensions.y});\n")
-                            elif "sphere" in object_type:
-                                final_lines.append(indentation + f"sphere({object.dimensions.x / 2}, 24, 24);\n")
-                            elif "cylinder" in object_type:
-                                final_lines.append(indentation + f"cylinder({object.dimensions.x / 2}, {object.dimensions.z}, 24, 1, true, true);\n")
-                            elif "cone" in object_type:
-                                final_lines.append(indentation + f"cone({object.dimensions.x / 2}, {object.dimensions.z}, 24, 1, true);\n")
-                            elif "ellipsoid" in object_type:
-                                final_lines.append(indentation + f"ellipsoid({object.dimensions.x / 2}, {object.dimensions.y / 2}, {object.dimensions.z / 2}, 24, 24);\n")
-                            elif "torus" in object_type:
-                                final_lines.append(indentation + f"torus({(object.dimensions.x - object.dimensions.z) / 2}, {object.dimensions.z / 2});\n")
-                            final_lines.append(indentation + "pop();\n")
-                            final_lines.append("\n")
-                        elif "light" in object.name:
-                            final_lines.append(indentation + f"pointLight(255, 255, 255, {-object.location.x}, {object.location.y}, {object.location.z});\n")
-                            final_lines.append("\n")
+            for object in collection.objects:
+                name_components = object.name.split("_")
+                obj = {}
+                if name_components[0] == 'camera':
+                    obj['type'] = 'camera'
+                    obj['name'] = object.name
+                    obj['location'] = [-object.location.x, object.location.y, object.location.z]
+                    camera_up = object.matrix_world.to_quaternion() @ Vector((0.0, -1.0, 0.0))
+                    obj['up'] = [-camera_up.x, camera_up.y, camera_up.z]
+                    camera_forward = object.matrix_world.to_quaternion() @ Vector((0.0, 0.0, -1.0))
+                    camera_target = object.location + camera_forward
+                    obj['target'] = [-camera_target.x, camera_target.y, camera_target.z]
+                elif name_components[0] == "obj" and len(name_components) > 1:
+                    obj_type = name_components[1]
+                    obj['type'] = obj_type
+                    obj['name'] = "_".join(name_components[2:])
+                    try:
+                        obj['color'] = [v * 255 for v in object.active_material.node_tree.nodes[1].inputs[0].default_value[:3]]
+                    except AttributeError:
+                        obj['color'] = [205, 205, 205]
+                    obj['color'].append(255) # alpha value
+                    obj['location'] = [-object.location.x, object.location.y, object.location.z]
+                    x_rotation = object.rotation_euler.x + (3.14159265 / 2 if obj_type == 'box' or obj_type == 'cylinder' or obj_type == 'cone' else 0)
+                    obj['rotation'] = [x_rotation, -object.rotation_euler.y, -object.rotation_euler.z]
+                    obj['scale'] = [1, 1, 1]
+                    if obj_type == 'box':
+                        obj['dimensions'] = [object.dimensions.x, object.dimensions.z, object.dimensions.y]
+                    elif obj_type == 'plane':
+                        obj['dimensions'] = [object.dimensions.x, object.dimensions.y]
+                    elif obj_type == 'sphere':
+                        obj['dimensions'] = [object.dimensions.x  / 2]
+                    elif obj_type == 'cylinder' or obj_type == 'cone':
+                        obj['dimensions'] = [object.dimensions.x / 2, object.dimensions.z]
+                    elif obj_type == 'ellipsoid':
+                        obj['dimensions'] = [object.dimensions.x / 2, object.dimensions.y / 2, object.dimensions.z / 2]
+                    elif obj_type == 'torus':
+                        obj['dimensions'] = [object.dimensions.x - object.dimensions.z, object.dimensions.z / 2]
+                elif name_components[0] == 'light':
+                    obj['type'] = 'light'
+                    obj['name'] = object.name
+                    obj['color'] = [255, 255, 255] # TODO: Make support for different colors
+                    obj['location'] = [-object.location.x, object.location.y, object.location.z]
+                else:
                     continue
-            final_lines.append(line)
+                final[collection.name].append(obj)
+            continue
                 
         with open(str(tool.output_dirpath) + str(tool.output_filename), "wt+") as f:
-            f.writelines(final_lines)
+            json.dump(final, f, indent=4)
 
         return {"FINISHED"}
 
