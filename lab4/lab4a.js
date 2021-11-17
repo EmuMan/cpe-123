82c543057d594aa2635d66f7a37044c644c66d46
EmuMan (Jacob Kelleran) <jacob.kelleran@outlook.com>
2021-11-17 04:47:07 -0800

    Finished lab4a and started lab4b

--- lab4/lab4a.js
+++ lab4/lab4a.js
@@ -39,14 +39,24 @@ class Tree extends P5Mesh {
    
    trunk;
    leaves;
+   fallingLeaves;
 
    constructor(name, location, rotation, scale, color) {
       super(name, location, rotation, scale, color);
       this.leaves = [];
+      this.fallingLeaves = [];
    }
 
    addLeaf(leaf) {
       this.leaves.push(leaf);
+   }
+
+   spawnFallingLeaf(vel, accel, ground) {
+      while (this.fallingLeaves.length >= 50) {
+         this.fallingLeaves.shift();
+      }
+      let leaf = this.leaves[Math.floor(Math.random() * this.leaves.length)];
+      this.fallingLeaves.push(new FallingLeaf('falling', this, leaf, vel, accel, ground));
    }
 
    drawMesh(instance) {
@@ -54,6 +64,58 @@ class Tree extends P5Mesh {
       for (let i = 0; i < this.leaves.length; i++) {
          this.leaves[i].addToScene(instance);
       }
+      for (let i = 0; i < this.fallingLeaves.length; i++) {
+         this.fallingLeaves[i].addToScene(instance);
+      }
+   }
+
+   update() {
+      for (let i = 0; i < this.fallingLeaves.length; i++) {
+         this.fallingLeaves[i].update();
+      }
+   }
+
+}
+
+class FallingLeaf extends P5Sphere {
+
+   vel;
+   accel;
+   maxVel;
+   ground;
+   tree;
+
+   constructor(name, tree, original, vel, accel, groundSphere) {
+      let scale = original.scale.copy();
+      scale.mult(1 / 2);
+      super(name, original.location.copy(), original.rotation.copy(), scale, original.color, original.radius);
+      this.vel = vel;
+      this.accel = accel;
+      this.maxVel = 0.5;
+      this.ground = groundSphere; // for collisions
+      this.tree = tree;
+      this.setDetail(4);
+   }
+
+   update() {
+      if (this.testCollision()) return; // incredibly janky, but i think the leaves are falling slowly enough for it to not matter
+
+      this.vel.add(this.accel);
+      if (Math.abs(this.vel.y) > this.maxVel) {
+         this.vel.y = this.maxVel * Math.sign(this.vel.y);
+      }
+      this.location.add(this.vel);
+   }
+
+   testCollision() {
+      let dist = this.ground.location.copy();
+      dist.sub(this.tree.location);
+      dist.sub(this.location);
+      let r2 = this.ground.radius * this.ground.radius;
+      if (dist.magSq() <= r2) {
+         return true;
+      }
+      return false;
    }
 
 }
@@ -63,12 +125,18 @@ let sketch = function(p) {
    let ground;
    let tree;
 
+   let leafWind = 0.2;
+   
+   let cloudCount = 150;
+   let cloudsRadius = 100;
+   let cloudSize = 25;
+
    function rotateVector(v, r) {
       // because apparently p5 doesn't natively provide this functionality for 3d vectors...
       // luckily, we can use a short - albeit fairly inefficient - trick to extend the 2d rotation into a 3rd dimension,
       // by simply performing it for every axis required (in xyz format) (trust me it works, i think)
       let tempR;
-      /*
+      //*
       tempR = p.createVector(v.x, v.y);
       tempR.rotate(r.z);
       v.y = tempR.y;
@@ -80,6 +148,8 @@ let sketch = function(p) {
       v.y = tempR.x;
       v.z = tempR.y;
       /*/
+      // more intuitive version
+
       tempR = p.createVector(v.y, v.z);
       tempR.rotate(r.x);
       v.y = tempR.x;
@@ -103,8 +173,17 @@ let sketch = function(p) {
       let newPosition = currentPosition.copy();
       newPosition.add(currentVector);
 
-      if (depth > -1) {
-         tree.addLeaf(new P5Sphere('leaf', newPosition, p.createVector(0, 0, 0), p.createVector(1, 1, 1), p.color(0, 255, 0), 2));
+      if (depth > 1) {
+         // This is not actually entirely accurate. I believe it has something to do with relative rotations and spaces, and
+         // theoretically it's fixable but that's not really practical here. Basically, it's close enough.
+         for (let i = 0; i < currentBranch.length; i++) {
+            let displacement = p5.Vector.random3D();
+            displacement.mult(currentBranch.length * 0.7);
+            displacement.add(newPosition);
+            let leaf = new P5Sphere('leaf', displacement, p.createVector(0, 0, 0), p.createVector(1, 1, 1), p.color(255, 173, 243, 200), 3);
+            leaf.setDetail(7);
+            tree.addLeaf(leaf);
+         }
       }
       
       if (depth == 4) return;
@@ -119,9 +198,6 @@ let sketch = function(p) {
       while ((p.random(0, 100) < branchChance && branchCount < maxBranches) || (depth < 3 && branchCount < 1)) {
          for (let i = 0; i < 3; i++) {
             rotations.push(i != 1 ? p.random(-rotationFactor, rotationFactor) : 0);
-            //rotations.push(i != -1 ? (i - 1) * p.PI / 4 : 0);
-            //rotations.push(i != 1 ? (i - 1) * (1 / (depth + 1)) * p.PI / 4 : 0);
-            //rotations.push(i != 0 ? (i + 1) * ((depth + 1) / 5) * p.PI / 8 : 0);
          }
          let rotation = p.createVector(rotations[0], rotations[1], rotations[2]);
          let length = currentBranch.length * p.random(0.7, 0.9);
@@ -130,25 +206,37 @@ let sketch = function(p) {
          let newVector = currentVector.copy();
          newVector.setMag(newBranch.length);
          rotateVector(newVector, rotation);
-         console.log(newVector);
          generateBranches(tree, depth + 1, newBranch, newVector, newPosition);
          branchCount += 1;
          rotations.splice(0, rotations.length); // clear array
       }
    }
 
+   function circularNoise(noiseScale, offset, time) {
+      // this is supposed to interpolate between the ends of a circle. i think it works?
+      let baseNoise = p.noise(noiseScale * offset, time);
+      let threshold = (7 / 4) * p.PI;
+      if (offset > threshold) {
+         let localized = offset - threshold;
+         let normalized = localized / (2 * p.PI - threshold);
+         let altNoise = p.noise(noiseScale * (localized - (2 * p.PI - threshold)), time);
+         baseNoise = (1 - normalized) * baseNoise + normalized * altNoise;
+      }
+      return baseNoise;
+   }
+
    p.setup = function() {
-      let cameraPos = p.createVector(0, 100, 0);
-      let cameraUp = p.createVector(0, 0, -1); // TODO: This should not have to be negative. Figure out why it does.
+      let cameraPos = p.createVector(0, 0, 100);
+      let cameraUp = p.createVector(0, -1, 0);
       let cameraFacing = p.createVector(0, 0, 0);
       sceneCamera = new P5Camera('main_camera', cameraPos, cameraUp, cameraFacing);
 
-      ground = new P5Sphere('ground', p.createVector(0, 0, -130), p.createVector(0, 0, 0), p.createVector(2, 1, 1), p.color(40, 181, 70), 100);
+      ground = new P5Sphere('ground', p.createVector(0, -130, 0), p.createVector(0, 0, 0), p.createVector(1, 1, 1), p.color(40, 181, 70), 100);
       ground.setDetail(64);
 
-      let treePos = p.createVector(0, 0, -30);
+      let treePos = p.createVector(0, -30, 0);
       let treeSize = 20;
-      tree = new Tree('tree', treePos, p.createVector(p.PI / 2, 0, 0), p.createVector(1, 1, 1), p.color(69, 41, 9));
+      tree = new Tree('tree', treePos, p.createVector(0, 0, 0), p.createVector(1, 1, 1), p.color(100, 60, 20));
       tree.trunk = new Branch(treeSize, 2, p.createVector(0, 0, 0));
       generateBranches(tree, 0, tree.trunk, p.createVector(0, treeSize, 0), p.createVector(0, 0, 0));
 
@@ -158,31 +246,44 @@ let sketch = function(p) {
    };
    
    p.draw = function() {
-      p.ambientLight(125, 125, 125);
-      p.directionalLight(p.color(255, 255, 255), p.createVector(-1, 0, -1))
+      let sceneRotation = p.frameCount / 360;
+      let lightDirection = p.createVector(1, 0, -1);
+      rotateVector(lightDirection, p.createVector(0, sceneRotation, 0));
+
+      p.ambientLight(165, 165, 165);
+      p.directionalLight(p.color(255, 255, 255), lightDirection)
 
       sceneCamera.addToScene(p);
 
       p.background(44, 184, 216);
 
-      //ground.addToScene(p);
+      p.rotateY(sceneRotation);
 
-      let rotVec = p.createVector(p.frameCount / 200, p.mouseX / 200, p.mouseY / 200);
+      ground.addToScene(p);
 
-      rotation = new Quaternion(-rotVec.x, rotVec.y, -rotVec.z);
+      p.fill(p.color(255, 255, 255, 220));
+      for (let i = 0; i < cloudCount; i++) {
+         let theta = (i / cloudCount) * (2 * p.PI);
+         p.push();
+         // lower layer
+            p.translate(p.cos(theta) * cloudsRadius, 50, p.sin(theta) * cloudsRadius);
+            let r = Math.max((circularNoise(2, theta, p.frameCount / 500) - 0.3) * cloudSize, 0);
+            if (r > 1.5) p.sphere(r, 5, 5);
+         p.pop();
+         p.push();
+            // upper layer
+            p.translate(p.cos(theta) * cloudsRadius * 0.8, 80, p.sin(theta) * cloudsRadius * 0.8);
+            r = Math.max((circularNoise(2, theta, p.frameCount / 500 + 50) - 0.3) * cloudSize, 0);
+            if (r > 1.5) p.sphere(r, 5, 5);
+         p.pop();
+      }
 
-      p.push();
-         let pos = p.createVector(10, 0, 0);
-         rotation.rotate(pos);
-         pos.add(p.createVector(0, 0, -30));
-         p.translate(pos);
-         p.box(10, 10, 10);
-      p.pop();
+      if (p.frameCount % 20 == 0) {
+         tree.spawnFallingLeaf(p.createVector(leafWind, 0, 0), p.createVector(0, -0.01, 0), ground);
+      }
 
-      p.push();
-         tree.rotation = rotVec;
-         tree.addToScene(p);
-      p.pop();
+      tree.update();
+      tree.addToScene(p);
    };
 
 };
--- lab4/lab4b.js
+++ lab4/lab4b.js
@@ -1,9 +1,72 @@
 // Starting code for Lab 4 - Generative Art
 // This should be your structured sketch
 
+let blockSize = 500;
+let blockSpace = 50;
+let buildingSpace = 10;
+
+function sum(l) {
+   return l.reduce((a, b) => a + b, 0);
+}
+
+class Block extends P5Mesh{
+
+   buildings;
+
+   constructor(location, rotation, scale, color) {
+
+      super('block', location, rotation, scale, color);
+      this.buildings = [];
+
+      let buildingSizes = [];
+      buildingSizes.push(buildingSpace);
+
+      while (sum(buildingSizes) < blockSize) {
+         buildingSizes.push(Math.random() * 50 + 50);
+         buildingSizes.push(buildingSpace);
+      }
+
+      totalLength = sum(buildingSizes);
+
+      bs2 = [...buildingSizes];
+      bs2.splice(buildingSizes.length - 2, 2);
+      length2 = sum(bs2);
+
+      // totalLength will be greater than blockSize, length2 will be less
+      if (totalLength - blockSize > blockSize - length2) {
+         totalLength = length2;
+         buildingSizes = bs2;
+      }
+
+      let scalar = blockSize / totalLength;
+      buildingSizes.map(e => e * scalar);
+
+      console.log(buildingSizes);
+      console.log(sum(buildingSizes));
+
+      let disp = 0;
+      for (let i = 0; i < buildingSizes; i++) {
+         if (i % 2 == 0) {
+            // this element only describes a space between buildings
+            disp += buildingSizes[i];
+            continue;
+         }
+         disp += buildingSizes / 2;
+         
+         disp += buildingSizes / 2;
+      }
+
+   }
+
+   drawMesh() {
+
+   }
+
+}
+
 function setup() 
 {
-   createCanvas(400, 400); // You may make this larger
+   createCanvas(400, 400);
 }
 
 function draw() 
