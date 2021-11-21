8f77f806a8677dce3b078b65527286d1c2467bdb
EmuMan <jacob.kelleran@outlook.com>
2021-11-20 18:29:35 -0800

    Copied files from lab4 to final-project; added incomplete physics engine to p5engine.js

---
+++ final-project/finalproject.js
@@ -0,0 +1,296 @@
+// Starting code for Lab 4 - Generative Art
+// This should be your organic sketch
+
+let cameraDistance = 100;
+let cameraSpeed = 1 / 360;
+
+let leafWind = 0.2;
+
+let cloudCount = 150;
+let cloudsRadius = 100;
+let cloudSize = 25;
+let cloudHeight = 50;
+
+class Branch {
+
+   length;
+   radius;
+   rotation;
+   children;
+
+   constructor(length, radius, rotation) {
+      this.length = length;
+      this.radius = radius;
+      this.rotation = rotation;
+      this.children = [];
+   }
+
+   addChild(child) {
+      this.children.push(child);
+   }
+
+   drawRecursive(instance) {
+      instance.push();
+         instance.rotateZ(this.rotation.z);
+         instance.rotateY(this.rotation.y);
+         instance.rotateX(this.rotation.x);
+         instance.translate(0, this.length / 2, 0);
+         instance.cylinder(this.radius, this.length);
+         instance.translate(0, this.length / 2, 0);
+         for (let i = 0; i < this.children.length; i++) {
+            this.children[i].drawRecursive(instance);
+         }
+      instance.pop();
+   }
+
+}
+
+class Tree extends P5Mesh {
+   
+   trunk;
+   leaves;
+   fallingLeaves;
+
+   constructor(name, location, rotation, scale, color) {
+      super(name, location, rotation, scale, color);
+      this.leaves = [];
+      this.fallingLeaves = [];
+   }
+
+   addLeaf(leaf) {
+      this.leaves.push(leaf);
+   }
+
+   spawnFallingLeaf(vel, accel, ground) {
+      while (this.fallingLeaves.length >= 50) {
+         this.fallingLeaves.shift();
+      }
+      let leaf = this.leaves[Math.floor(Math.random() * this.leaves.length)];
+      this.fallingLeaves.push(new FallingLeaf('falling', this, leaf, vel, accel, ground));
+   }
+
+   drawMesh(instance) {
+      this.trunk.drawRecursive(instance);
+      for (let i = 0; i < this.leaves.length; i++) {
+         this.leaves[i].addToScene(instance);
+      }
+      for (let i = 0; i < this.fallingLeaves.length; i++) {
+         this.fallingLeaves[i].addToScene(instance);
+      }
+   }
+
+   update() {
+      for (let i = 0; i < this.fallingLeaves.length; i++) {
+         this.fallingLeaves[i].update();
+      }
+   }
+
+}
+
+class FallingLeaf extends P5Sphere {
+
+   vel;
+   accel;
+   maxVel;
+   ground;
+   tree;
+
+   constructor(name, tree, original, vel, accel, groundSphere) {
+      let scale = original.scale.copy();
+      scale.mult(1 / 2);
+      super(name, original.location.copy(), original.rotation.copy(), scale, original.color, original.radius);
+      this.vel = vel;
+      this.accel = accel;
+      this.maxVel = 0.5;
+      this.ground = groundSphere; // for collisions
+      this.tree = tree;
+      this.setDetail(4);
+   }
+
+   update() {
+      if (this.testCollision()) return; // incredibly janky, but i think the leaves are falling slowly enough for it to not matter
+
+      this.vel.add(this.accel);
+      if (Math.abs(this.vel.y) > this.maxVel) {
+         this.vel.y = this.maxVel * Math.sign(this.vel.y);
+      }
+      this.location.add(this.vel);
+   }
+
+   testCollision() {
+      let dist = this.ground.location.copy();
+      dist.sub(this.tree.location);
+      dist.sub(this.location);
+      let r2 = this.ground.radius * this.ground.radius;
+      if (dist.magSq() <= r2) {
+         return true;
+      }
+      return false;
+   }
+
+}
+
+let sketch = function(p) {
+
+   let ground;
+   let tree;
+
+   function rotateVector(v, r) {
+      // because apparently p5 doesn't natively provide this functionality for 3d vectors...
+      // luckily, we can use a short - albeit fairly inefficient - trick to extend the 2d rotation into a 3rd dimension,
+      // by simply performing it for every axis required (in xyz format) (trust me it works, i think)
+      let tempR;
+      //*
+      tempR = p.createVector(v.x, v.y);
+      tempR.rotate(r.z);
+      v.y = tempR.y;
+      tempR.y = v.z;
+      tempR.rotate(-r.y);
+      v.x = tempR.x;
+      tempR.x = v.y;
+      tempR.rotate(r.x);
+      v.y = tempR.x;
+      v.z = tempR.y;
+      /*/
+      // more intuitive version
+
+      tempR = p.createVector(v.y, v.z);
+      tempR.rotate(r.x);
+      v.y = tempR.x;
+      v.z = tempR.y;
+
+      tempR = p.createVector(v.x, v.z);
+      tempR.rotate(-r.y);
+      v.x = tempR.x;
+      v.z = tempR.y;
+      
+      tempR = p.createVector(v.x, v.y);
+      tempR.rotate(r.z);
+      v.x = tempR.x;
+      v.y = tempR.y;
+      //*/
+
+   }
+
+   function generateBranches(tree, depth, currentBranch, currentVector, currentPosition) {
+
+      let newPosition = currentPosition.copy();
+      newPosition.add(currentVector);
+
+      if (depth > 1) {
+         // This is not actually entirely accurate. I believe it has something to do with relative rotations and spaces, and
+         // theoretically it's fixable but that's not really practical here. Basically, it's close enough.
+         for (let i = 0; i < currentBranch.length; i++) {
+            let displacement = p5.Vector.random3D();
+            displacement.mult(currentBranch.length * 0.7);
+            displacement.add(newPosition);
+            let leaf = new P5Sphere('leaf', displacement, p.createVector(0, 0, 0), p.createVector(1, 1, 1), p.color(255, 173, 243, 200), 3);
+            leaf.setDetail(7);
+            tree.addLeaf(leaf);
+         }
+      }
+      
+      if (depth == 4) return;
+
+      let branchChance = 75; // percent chance that a branch will form every attempt
+      let maxBranches = 3;
+      let rotationFactor = p.PI / 4;
+
+      let branchCount = 0;
+      let rotations = [];
+
+      while ((p.random(0, 100) < branchChance && branchCount < maxBranches) || (depth < 3 && branchCount < 1)) {
+         for (let i = 0; i < 3; i++) {
+            rotations.push(i != 1 ? p.random(-rotationFactor, rotationFactor) : 0);
+         }
+         let rotation = p.createVector(rotations[0], rotations[1], rotations[2]);
+         let length = currentBranch.length * p.random(0.7, 0.9);
+         let newBranch = new Branch(length, currentBranch.radius * 0.8, rotation);
+         currentBranch.addChild(newBranch);
+         let newVector = currentVector.copy();
+         newVector.setMag(newBranch.length);
+         rotateVector(newVector, rotation);
+         generateBranches(tree, depth + 1, newBranch, newVector, newPosition);
+         branchCount += 1;
+         rotations.splice(0, rotations.length); // clear array
+      }
+   }
+
+   function circularNoise(noiseScale, offset, time) {
+      // this is supposed to interpolate between the ends of a circle. i think it works?
+      let baseNoise = p.noise(noiseScale * offset, time);
+      let threshold = (7 / 4) * p.PI;
+      if (offset > threshold) {
+         let localized = offset - threshold;
+         let normalized = localized / (2 * p.PI - threshold);
+         let altNoise = p.noise(noiseScale * (localized - (2 * p.PI - threshold)), time);
+         baseNoise = (1 - normalized) * baseNoise + normalized * altNoise;
+      }
+      return baseNoise;
+   }
+
+   p.setup = function() {
+      let cameraPos = p.createVector(0, 0, cameraDistance);
+      let cameraUp = p.createVector(0, -1, 0);
+      let cameraFacing = p.createVector(0, 0, 0);
+      sceneCamera = new P5Camera('main_camera', cameraPos, cameraUp, cameraFacing);
+
+      ground = new P5Sphere('ground', p.createVector(0, -130, 0), p.createVector(0, 0, 0), p.createVector(1, 1, 1), p.color(40, 181, 70), 100);
+      ground.setDetail(64);
+
+      let treePos = p.createVector(0, -30, 0);
+      let treeSize = 20;
+      tree = new Tree('tree', treePos, p.createVector(0, 0, 0), p.createVector(1, 1, 1), p.color(100, 60, 20));
+      tree.trunk = new Branch(treeSize, 2, p.createVector(0, 0, 0));
+      generateBranches(tree, 0, tree.trunk, p.createVector(0, treeSize, 0), p.createVector(0, 0, 0));
+
+      p.pixelDensity(1);
+      p.createCanvas(400, 400, p.WEBGL);
+      p.noStroke();
+   };
+   
+   p.draw = function() {
+      let cameraPos = p.createVector(0, 0, cameraDistance);
+      rotateVector(cameraPos, p.createVector(0, -p.frameCount * cameraSpeed, 0));
+      sceneCamera.location = cameraPos;
+
+      p.ambientLight(165, 165, 165);
+      p.directionalLight(p.color(255, 255, 255), p.createVector(1, 0, -1))
+
+      sceneCamera.addToScene(p);
+
+      p.background(44, 184, 216);
+
+      ground.addToScene(p);
+
+      p.fill(p.color(255, 255, 255, 220));
+      for (let i = 0; i < cloudCount; i++) {
+         let theta = (i / cloudCount) * (2 * p.PI);
+         p.push();
+            // lower layer
+            let noiseVal = circularNoise(2, theta, p.frameCount / 500);
+            p.translate(p.cos(theta) * cloudsRadius, cloudHeight + noiseVal * cloudSize / 2, p.sin(theta) * cloudsRadius);
+            let r = Math.max((noiseVal - 0.3) * cloudSize, 0);
+            if (r > 1.8) p.sphere(r, 5, 5);
+         p.pop();
+         p.push();
+            // upper layer
+            noiseVal = circularNoise(2, theta, p.frameCount / 500 + 50);
+            p.translate(p.cos(theta) * cloudsRadius * 0.8, cloudHeight + 30 + noiseVal * cloudSize / 2, p.sin(theta) * cloudsRadius * 0.8);
+            r = Math.max((circularNoise(2, theta, p.frameCount / 500 + 50) - 0.3) * cloudSize, 0);
+            if (r > 2.0) p.sphere(r, 5, 5);
+         p.pop();
+      }
+
+      if (p.frameCount % 20 == 0) {
+         tree.spawnFallingLeaf(p.createVector(leafWind, 0, 0), p.createVector(0, -0.01, 0), ground);
+      }
+
+      tree.update();
+      tree.addToScene(p);
+   };
+
+};
+
+
+let instance = new p5(sketch);
---
+++ final-project/p5engine.js
@@ -0,0 +1,502 @@
+class Quaternion {
+
+    // https://api.flutter.dev/flutter/vector_math/Quaternion-class.html
+
+    x;
+    y;
+    z;
+    w;
+
+    constructor(x, y, z, w) {
+        if (w === undefined) {
+            this.setEuler(x, y, z);
+        } else {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.w = w;
+        }
+    }
+
+    add(other) {
+        this.x += other.x;
+        this.y += other.y;
+        this.z += other.z;
+        this.w += other.w;
+    }
+
+    copy() {
+        return new Quaternion(this.x, this.y, this.z, this.w);
+    }
+
+    length() {
+        return Math.sqrt(this.lengthSq())
+    }
+
+    lengthSq() {
+        return this.x * this.x + this.y * this.y + this.z * this.z + this.w * this.w;
+    }
+
+    normalize() {
+        let l = this.length();
+        if (l == 0.0) return;
+        
+        let d = 1.0 / l;
+        this.x *= l;
+        this.y *= l;
+        this.z *= l;
+        this.w *= l;
+    }
+
+    rotate(v) {
+        // god help me
+
+        // conjugate(this) * [v,0] * this
+        
+        let tiw = this.w;
+        let tiz = -this.z;
+        let tiy = -this.y;
+        let tix = -this.x;
+        
+        let tx = tiw * v.x + tix * 0.0 + tiy * v.z - tiz * v.y;
+        let ty = tiw * v.y + tiy * 0.0 + tiz * v.x - tix * v.z;
+        let tz = tiw * v.z + tiz * 0.0 + tix * v.y - tiy * v.x;
+        let tw = tiw * 0.0 - tix * v.x - tiy * v.y - tiz * v.z;
+        
+        v.x = tw * this.x + tx * this.w + ty * this.z - tz * this.y;
+        v.y = tw * this.y + ty * this.w + tz * this.x - tx * this.z;
+        v.z = tw * this.z + tz * this.w + tx * this.y - ty * this.x;
+    }
+
+    setEuler(yaw, pitch, roll) {
+        let halfYaw = yaw * 0.5;
+        let halfPitch = pitch * 0.5;
+        let halfRoll = roll * 0.5;
+
+        let cosYaw = Math.cos(halfYaw);
+        let sinYaw = Math.sin(halfYaw);
+        let cosPitch = Math.cos(halfPitch);
+        let sinPitch = Math.sin(halfPitch);
+        let cosRoll = Math.cos(halfRoll);
+        let sinRoll = Math.sin(halfRoll);
+
+        this.x = cosRoll * sinPitch * cosYaw + sinRoll * cosPitch * sinYaw;
+        this.y = cosRoll * cosPitch * sinYaw - sinRoll * sinPitch * cosYaw;
+        this.z = sinRoll * cosPitch * cosYaw - cosRoll * sinPitch * sinYaw;
+        this.w = cosRoll * cosPitch * cosYaw + sinRoll * sinPitch * sinYaw;
+    }
+
+}
+
+class P5Object {
+
+    name;
+    location;
+
+    constructor(name, location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    addToScene(instance) { }
+
+    move(x, y, z) {
+        this.location.x += x;
+        this.location.y += y;
+        this.location.z += z;
+    }
+
+}
+
+class P5Camera extends P5Object {
+
+    up;
+    target;
+
+    constructor(name, location, up, target) {
+        super(name, location);
+        this.up = up;
+        this.target = target;
+    }
+
+    addToScene(instance) {
+        instance.camera(this.location.x, this.location.y, this.location.z,
+            this.target.x, this.target.y, this.target.z,
+            this.up.x, this.up.y, this.up.z);
+    }
+
+}
+
+class P5Mesh extends P5Object {
+
+    rotation;
+    scale;
+    color;
+    children;
+    outline;
+
+    constructor(name, location, rotation, scale, color, outline) {
+        super(name, location);
+        this.rotation = rotation;
+        this.scale = scale;
+        this.color = color;
+        this.children = [];
+        if (outline) this.outline = outline; else this.outline = 0;
+    }
+
+    addChild(obj) {
+        this.children.push(obj);
+    }
+
+    addToScene(instance) {
+        instance.push();
+            instance.translate(this.location.x, this.location.y, this.location.z);
+            instance.scale(this.scale.x, this.scale.y, this.scale.z);
+            instance.rotateZ(this.rotation.z);
+            instance.rotateY(this.rotation.y);
+            instance.rotateX(this.rotation.x);
+            instance.fill(this.color);
+            instance.strokeWeight(this.outline);
+            this.drawMesh(instance);
+            this.children.forEach(c => c.addToScene(instance));
+        instance.pop();
+    }
+
+    drawMesh() { }
+
+}
+
+class P5Box extends P5Mesh {
+
+    dimensions;
+
+    constructor(name, location, rotation, scale, color, dimensions, outline) {
+        super(name, location, rotation, scale, color, outline);
+        this.dimensions = dimensions;
+    }
+
+    drawMesh(instance) { instance.box(this.dimensions.x, this.dimensions.y, this.dimensions.z); }
+
+}
+
+class P5Plane extends P5Mesh {
+
+    dimensions;
+
+    constructor(name, location, rotation, scale, color, dimensions, outline) {
+        super(name, location, rotation, scale, color, outline);
+        this.dimensions = dimensions;
+    }
+
+    drawMesh(instance) { instance.plane(this.dimensions.x, this.dimensions.y); }
+
+}
+
+class P5Sphere extends P5Mesh {
+
+    radius;
+    detailX;
+    detailY;
+
+    constructor(name, location, rotation, scale, color, radius, outline) {
+        super(name, location, rotation, scale, color, outline);
+        this.radius = radius;
+        this.detailX = this.detailY = 16;
+    }
+
+    setDetail(detail) { this.detailX = this.detailY = detail; }
+
+    drawMesh(instance) { instance.sphere(this.radius, this.detailX, this.detailY); }
+
+}
+
+class P5Cylinder extends P5Mesh {
+
+    radius;
+    height;
+
+    constructor(name, location, rotation, scale, color, radius, height, outline) {
+        super(name, location, rotation, scale, color, outline);
+        this.radius = radius;
+        this.height = height;
+    }
+
+    drawMesh(instance) { instance.cylinder(this.radius, this.height, 16, 1, true, true); }
+
+}
+
+class P5Cone extends P5Mesh {
+
+    radius;
+    height;
+
+    constructor(name, location, rotation, scale, color, radius, height, outline) {
+        super(name, location, rotation, scale, color, outline);
+        this.radius = radius;
+        this.height = height;
+    }
+
+    drawMesh(instance) { instance.cone(this.radius, this.height, 16, 1, true); }
+
+}
+
+class P5Ellipsoid extends P5Mesh {
+    radii;
+
+    constructor(name, location, rotation, scale, color, radii, outline) {
+        super(name, location, rotation, scale, color, outline);
+        this.radii = radii;
+    }
+
+    drawMesh(instance) { instance.ellipsoid(this.radii.x, this.radii.y, this.radii.z, 24, 24); }
+
+}
+
+class P5Torus extends P5Mesh {
+
+    radius;
+    tubeRadius;
+
+    constructor(name, location, rotation, scale, color, radius, tubeRadius, outline) {
+        super(name, location, rotation, scale, color, outline);
+        this.radius = radius;
+        this.tubeRadius = tubeRadius;
+    }
+
+    drawMesh(instance) { instance.torus(this.radius, this.tubeRadius, 24, 16); }
+
+}
+
+// a2v = array to vector
+function a2v(a) {
+    if (a.length == 2) {
+        return new p5.Vector(a[0], a[1]);
+    } else {
+        return new p5.Vector(a[0], a[1], a[2]);
+    }
+}
+
+function loadObject(data, instance, color) {
+    if (instance && !color && data['type'] != 'camera') {
+        color = instance.color(data['color']);
+    }
+    switch(data['type']) {
+        case 'camera':
+            return new P5Camera(data['name'], a2v(data['location']), a2v(data['up']), a2v(data['target']))
+        case 'box':
+            return new P5Box(data['name'], a2v(data['location']), a2v(data['rotation']), a2v(data['scale']), color, a2v(data['dimensions']));
+        case 'plane':
+            return new P5Plane(data['name'], a2v(data['location']), a2v(data['rotation']), a2v(data['scale']), color, a2v(data['dimensions']));
+        case 'sphere':
+            return new P5Sphere(data['name'], a2v(data['location']), a2v(data['rotation']), a2v(data['scale']), color, data['dimensions'][0]);
+        case 'cylinder':
+            return new P5Cylinder(data['name'], a2v(data['location']), a2v(data['rotation']), a2v(data['scale']), color, data['dimensions'][0], data['dimensions'][1]);
+        case 'cone':
+            return new P5Cone(data['name'], a2v(data['location']), a2v(data['rotation']), a2v(data['scale']), color, data['dimensions'][0], data['dimensions'][1]);
+        case 'ellipsoid':
+            return new P5Ellipsoid(data['name'], a2v(data['location']), a2v(data['rotation']), a2v(data['scale']), color, a2v(data['dimensions']));
+        case 'torus':
+            return new P5Torus(data['name'], a2v(data['location']), a2v(data['rotation']), a2v(data['scale']), color, data['dimensions'][0], data['dimensions'][1]);
+    }
+    return null;
+}
+
+
+class Ray {
+
+    origin;
+    direction;
+
+    constructor(origin, direction) {
+        this.origin = origin;
+        this.direction = direction;
+    }
+
+}
+
+class RayHit {
+    
+    ray;
+    time;
+    normal;
+
+    constructor(ray, time, normal) {
+        this.ray = ray;
+        this.time = time;
+        this.normal = normal;
+    }
+
+}
+
+class Trigger {
+
+    name;
+    location;
+
+    constructor(name, location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    testPoint(point) { }
+
+    testRay(ray) { }
+
+}
+
+class SphereTrigger extends Trigger {
+
+    radius;
+
+    constructor(name, location, radius) {
+        super(name, location);
+        this.radius = radius;
+    }
+
+    testPoint(point) {
+        let distance = point.copy();
+        distance.sub(this.location);
+        // TODO: Try the below line when possible
+        // let distance = p5.Vector.sub(point, this.location)
+        return distance.magSq() < this.radius * this.radius;
+    }
+
+    testRay(ray) {
+        // TODO: Implement
+    }
+
+}
+
+class BoxTrigger extends Trigger {
+
+    dimensions;
+
+    constructor(name, location, dimensions) {
+        super(name, location);
+        this.dimensions = dimensions;
+    }
+
+    testPoint(point) {
+        return point.x > this.location.x - this.dimensions.x / 2 &&
+               point.x < this.location.x + this.dimensions.x / 2 &&
+               point.y > this.location.x - this.dimensions.y / 2 &&
+               point.y < this.location.x + this.dimensions.y / 2 &&
+               point.z > this.location.x - this.dimensions.z / 2 &&
+               point.z < this.location.x + this.dimensions.z / 2
+    }
+
+    testRay(point) {
+        // TODO: Implement
+    }
+
+}
+
+class CollisionResolution {
+
+    dynamic;
+    static;
+    deltaTime; // used to scale velocity to dT
+    time; // used to determine where on the scaled velocity the collision hit
+    normal;
+
+    constructor(dynamic, static, deltaTime, time, normal) {
+        this.dynamic = dynamic;
+        this.static = static;
+        this.deltaTime = deltaTime;
+        this.time = time;
+        this.normal = normal;
+    }
+
+    resolve() {
+        let scaledVel = this.dynamic.velocity.copy();
+        scaledVel.mult(this.deltaTime);
+
+        // r = resolution
+        let rMag = scaledVel.dot(this.normal) * (1 - this.time);
+        let rVec = this.normal.copy();
+        rVec.mult(rMag);
+
+        this.dynamic.location.sub(rVec);
+    }
+
+}
+
+class DynamicCollider extends BoxTrigger {
+
+    velocity;
+    forces;
+
+    constructor(name, location, dimensions) {
+        super(name, location, dimensions);
+        this.velocity = 0;
+        this.forces = [];
+    }
+
+    testCollision(other, deltaTime) {
+        let _velocity = this.velocity.copy();
+        _velocity.mult(deltaTime);
+        // TODO: Implement, will return a CollisionResult
+    }
+
+    addForce(force) {
+        this.forces.push(force);
+    }
+
+    applyForces(deltaTime) {
+        this.forces.forEach(f => {
+            _force = f.copy();
+            _force.mult(deltaTime);
+            this.velocity.add(_force);
+        });
+    }
+
+}
+
+class Physics {
+
+    gravity;
+
+    triggers;
+    staticColliders;
+    dynamicColliders;
+
+    constructor(gravity) {
+        this.gravity = gravity;
+
+        this.triggers = [];
+        this.staticColliders = [];
+        this.dynamicColliders = [];
+    }
+
+    update(deltaTime) {
+        this.dynamicColliders.forEach(dc => {
+            dc.addForce(this.gravity);
+            dc.applyForces();
+
+            let collisions = [];
+            this.staticColliders.forEach(sc => {
+                collisions.push(dc.testCollision(sc, deltaTime));
+            });
+            collisions.sort((a, b) => a.time - b.time);
+
+            dc.location.add(dc.velocity);
+
+            collisions.forEach(col => {
+                col.resolve();
+            });
+        });
+    }
+
+    addTrigger(trigger) {
+        this.triggers.push(trigger);
+    }
+
+    addStaticCollider(collider) {
+        this.staticColliders.push(collider);
+    }
+
+    addDynamicCollider(collider) {
+        this.dynamicColliders.push(collider);
+    }
+
+}
